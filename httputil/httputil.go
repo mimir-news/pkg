3346b8ab80eb4f2e4dbf60db22255e5b6ccbb873
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-11-13 21:03:07 +0100

    Added additional response and request utitilites

--- httputil/httputil.go
+++ httputil/httputil.go
@@ -1,16 +1,61 @@
 package httputil
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+// HealthCheck function signature of a health check.
+type HealthCheck func() error
+
 // NewRouter creates a default router.
-func NewRouter(name, version string) *gin.Engine {
+func NewRouter(name, version string, check HealthCheck) *gin.Engine {
 	r := gin.Default()
 	r.Use(
 		ServerInfo(name, version),
 		RequestID(),
 		HandleErrors())
 
+	r.GET("/health", createHealthCheckHandler(check))
 	return r
+}
+
+// SendOK sends an ok status and message to the client.
+func SendOK(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+}
+
+// ParseQueryValue parses a query value from request.
+func ParseQueryValue(c *gin.Context, key string) (string, error) {
+	value, ok := c.GetQuery(key)
+	if !ok {
+		errMsg := fmt.Sprintf("No value found for param: %s", key)
+		return "", NewError(errMsg, http.StatusBadRequest)
+	}
+	return value, nil
+}
+
+// ParseQueryValues parses query values from a request.
+func ParseQueryValues(c *gin.Context, key string) ([]string, error) {
+	values, ok := c.GetQueryArray(key)
+	if !ok {
+		errMsg := fmt.Sprintf("No value found for param: %s", key)
+		return nil, NewError(errMsg, http.StatusBadRequest)
+	}
+	return values, nil
+}
+
+func createHealthCheckHandler(check HealthCheck) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := check()
+		if err != nil {
+			httpErr := NewError(err.Error(), http.StatusServiceUnavailable)
+			c.Error(httpErr)
+			return
+		}
+
+		SendOK(c)
+	}
 }
